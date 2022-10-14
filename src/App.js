c44c83100a3be720a3c697b411404196381e4bbc
tyz2019 <donal.tong@gmail.com>
2022-10-14 15:55:40 +0800

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import OfdView from "ofd-view-react"
+import 'ofd-view-react/dist/viewer.css'
+import {
+    openOFDViewer
+} from 'ofd.js'
+
+function configCallback(config) {
+    console.log(config)
+    openOFDViewer(config)
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <OfdView onConfigCallback={configCallback}></OfdView>
     </div>
   );
 }
