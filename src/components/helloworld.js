44727c32cdf81125a5d1367b8689e7c653b19429
tyz2019 <2238211+DLTech21@users.noreply.github.com>
2023-05-10 17:06:55 +0800

    pdf

--- src/components/helloworld.js
+++ src/components/helloworld.js
@@ -1,27 +1,31 @@
 import React, { Component } from 'react';
-import OfdView from "ofd-view-react"
-import 'ofd-view-react/dist/viewer.css'
-const mem = require('ofd.js')
+import PdfView from "pdf-view-react"
+import 'pdf-view-react/dist/viewer.css'
+const mem = require('mpdf.js')
 
 export default class HelloWorld extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            file: ''
+            file: '',
+            wasmUrl: ''
         }
     }
 
 
     componentDidMount() {
-        this.setState({
-            file: 'https://gitee.com/ofdrw/ofdrw/raw/master/ofdrw-converter/src/test/resources/999.ofd'
-        })
+        setTimeout(()=>{
+            this.setState({
+                file: ''
+            })
+        }, 500)
+
     }
 
     render() {
         return (
             <div className="App">
-                <OfdView file={this.state.file} mem={mem}></OfdView>
+                <PdfView file={this.state.file} mem={mem} wasmUrl={this.state.wasmUrl}></PdfView>
             </div>
         );
     }
